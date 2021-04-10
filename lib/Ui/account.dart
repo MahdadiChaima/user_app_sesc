424c9f3478183fbf35bfa27987f4423133134b49
amina04 <khachaamina0@gmail.com>
2021-04-10 18:07:49 +0200

    account mazal ykhoss ta3 updatepassword dok ndirha

--- lib/Ui/account.dart
+++ lib/Ui/account.dart
@@ -5,6 +5,9 @@ import 'package:user_app_sesc/Ui/ConstantWidget/appBarwithiconMenu.dart';
 import 'package:user_app_sesc/Ui/ConstantWidget/inputField.dart';
 import 'package:user_app_sesc/Ui/Home.dart';
 import 'package:user_app_sesc/Ui/SingIn.dart';
+import 'package:user_app_sesc/Ui/Drawer/DrawerScreen.dart';
+import 'package:user_app_sesc/Ui/Drawer/DrawerScreen.dart';
+
 import 'ConstantWidget/Bottom.dart';
 final TextEditingController name = new TextEditingController();
 final TextEditingController inscription_number = new TextEditingController();
@@ -17,39 +20,121 @@ class account extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size=MediaQuery.of(context).size;
+    GlobalKey <ScaffoldState> _scaffoldKey =GlobalKey<ScaffoldState>();
     return SafeArea(
+
       child: Scaffold(
+        key: _scaffoldKey,
+        drawer: Drawer(
+          child:drawer(),
+        ),
         body: ListView(
 
           children: [
-            //appBarwithiconMenu("Account"),
-            SizedBox(height: size.width/7,),
-            Container(margin: EdgeInsets.only(left:size.width/30 ),alignment:Alignment.topLeft,child:Text("First and last name:",style: TextStyle(color: colorUser.gray,fontSize: size.width/22),)),
+            Stack(children: [ Container(
 
-            inputField(contrl: name),
-            SizedBox(height: size.width/10 ,),
-            Container(margin: EdgeInsets.only(left:size.width/30 ),alignment:Alignment.topLeft,child: Text("Insccription Number:",style: TextStyle(color: colorUser.gray,fontSize: size.width/22),)),
+              width: size.width,height: size.width/1.4,
 
-            inputField(contrl:inscription_number),
-            SizedBox(height: size.width/10 ,),
-            Container(margin: EdgeInsets.only(left:size.width/30 ),alignment:Alignment.topLeft,child: Text("Email:",style: TextStyle(color: colorUser.gray,fontSize: size.width/22),)),
+              child: Column(
+                children: [
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(40.0),
+                      child: CircleAvatar(
+                        backgroundImage: AssetImage("assest/user.jpg"),
+                        radius: size.width/6.2,
+                        foregroundColor: Colors.deepOrange,
 
-            inputField(contrl:email),
-            SizedBox(height: size.width/10 ,),
-            Container(margin: EdgeInsets.only(left:size.width/30 ),alignment:Alignment.topLeft,child: Text("Password:",style: TextStyle(color: colorUser.gray,fontSize: size.width/22),)),
+                      ),
+                    ),
+                  ),
 
-            inputField(contrl:password),
-            SizedBox(height: size.width/5 ,),
+                  Center(
+                    // TODO: tet3amer a partir la bd .
+                    child: Text('Amina Khacha',style: TextStyle(
+                      color: Colors.white,
+                      fontSize: size.width/11,
+                      fontWeight: FontWeight.bold,
 
-             bottom(
+                    ),),
+                  )
+                ],
+              ),
+              decoration: BoxDecoration(
+                  gradient:LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [colorUser.lightBlue, colorUser.darkBlue]),
+               //  color: Colors.white,
+                  borderRadius: BorderRadius.only(bottomRight: Radius.circular(60) ,bottomLeft: Radius.circular(60) ,
+                  ),boxShadow: [BoxShadow(
+                offset: Offset(1,0),
+                color: colorUser.darkGray,blurRadius: 14,)]),),
+              Container(margin: EdgeInsets.only(right:size.width/30,top: size.width/30 ),alignment:Alignment.topLeft
+                  ,child:IconButton(onPressed:  ()=>_scaffoldKey.currentState.openDrawer(),
+                      icon:Icon(Icons.menu_outlined,size: size.width/10,color: Colors.white,))),
+              Container(margin: EdgeInsets.only(left:size.width/30,top: size.width/30 ),alignment:Alignment.topRight,
+                  child:IconButton(onPressed: ()=>{Navigator.pushNamed(context,Home.id)},icon:
+                  Icon(Icons.home_filled,size: size.width/10,color: Colors.white,))),],),
+            SizedBox(
+              height: size.height/10,
+            ),
 
-                label: 'Save',
-                ontap: () {
-                  Navigator.pushNamed(context,Home.id);
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+              child: ListTile(
+                leading: Icon(
+                  Icons.mail_outline,
+                  color: colorUser.lightBlue,
+                ),
+                // TODO: tet3amer a partir la bd email.
+                title: Text(
+                  'aminakh@gmail.com',
+                  style: TextStyle(
+                    color: colorUser.darkGray,
+                    //  fontFamily: 'ionicons',
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+              child: ListTile(
+                leading: Icon(
+                  Icons.assignment_ind_outlined,
+                  color: colorUser.lightBlue,
+                ),
+                // TODO: tet3amer a partir la bd email.
+                title: Text(
+                  '171735057399',
+                  style: TextStyle(
+                    color: colorUser.darkGray,
+                    //  fontFamily: 'ionicons',
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: size.height/10,
+            ),
+
+            Center(
+              child: GestureDetector(
+                child:Text(
+                  'update your password',
+                  style: TextStyle(
+                      fontWeight:FontWeight.w600,color: colorUser.darkBlue,fontSize: size.width/22
+                  ),
+                ),
+                onTap:() {
+                  // TODO: alert of changin mdps
                 },
               ),
-
-
+            )
 
           ],
         ),
